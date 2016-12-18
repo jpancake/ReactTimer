5aed499c44492ced5cf40960ded1ee73f7c787fc
jpancake <jrazon_18@yahoo.com>
2016-12-17 23:03:11 -0800

    Add automatic tests (karma, mocha)

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,10 @@
+/**
+ * Created by PANCAK3 on 12/17/2016.
+ */
+import expect from 'expect';
+
+describe('App', () => {
+    it('should properly run tests', () => {
+        expect(1).toBe(1);
+    });
+});
