943fcef920bacfe75f1ce2d99de4e18650d90fca
jpancake <jrazon_18@yahoo.com>
2016-12-17 21:51:58 -0800

    Editted server.js for heroku

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const PORT = process.env.PORT || 3000;
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug', {pretty: true});
 
-app.get('/', function(req, res){
-    res.render('index');
-});
-
 app.use(function(req, res, next){
     if (req.headers['x-forwarded-proto'] === 'https'){
         res.redirect('http://' + req.hostname + req.url);
     }else{
         next();
     }
+});
+
+app.get('/', function(req, res){
+    res.render('index');
 });
 
 app.use(express.static('public'));
